c49ff73049d42c6e64eb23a277961a3a22686f38
StellarWarp <lsxestelle@icloud.com>
2025-01-04 17:07:40 +0800

    [project]

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,15 @@
 add_rules('mode.debug', 'mode.release')
 
+includes('HybridECS/src')
+
 target('HybridECS')
     set_languages('c++23')
     set_kind('binary')
     add_files('HybridECS/src/*.cpp')
-    
+
+
+includes('HybridECS/test_util')
+target('Test')
+    set_languages('c++23')
+    set_kind('binary')
+    add_files('Test/src/*.cpp')
